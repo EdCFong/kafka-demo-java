e69373bea736c5f1a8b595567b8c15007cbdca2e
Eduardo Castillo Fong <EDUARDCF90@GMAIL.COM>
2023-03-26 22:52:33 -0500

    Added the producer

--- kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
@@ -1,7 +1,40 @@
 package io.conduktor.demos.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
 public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
     public static void main(String[] args) {
-        System.out.println("hello");
+        log.info("hello");
+
+        //Create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers","127.0.0.1:9092");
+
+        //Set producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        //Create the Producer
+        KafkaProducer<String,String> producer = new KafkaProducer<>(properties);
+
+        //Create a producer record
+        ProducerRecord<String,String> producerRecord = new ProducerRecord<>("demo_java", "Hello world");
+
+        //Send Data
+        producer.send(producerRecord);
+
+        //Tell the producer to send all data and block until done -- synchronous
+        producer.flush();
+
+        //Flush and close the producer -- close also call flush
+        producer.close();
     }
 }
