c8988a842ad032c3a82163ca71ea396bf104afc5
Eduardo Castillo Fong <EDUARDCF90@GMAIL.COM>
2023-07-05 23:09:14 -0500

    Cleaning

--- kafka-basics/src/main/java/io/conduktor/demos/kafka/ConsumerDemo.java
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ConsumerDemo.java
@@ -1,8 +1,14 @@
 package io.conduktor.demos.kafka;
 
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-jnryk
+
+import java.time.Duration;
+import java.util.Arrays;
 import java.util.Properties;
 
 public class ConsumerDemo {
@@ -19,10 +25,10 @@ public class ConsumerDemo {
         properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
 
         // create consumer configs
-        properties.setProperties("key.deserializer", StringDeserializer.class.getName());
-        properties.setProperties("value.deserializer", StringDeserializer.class.getName());
-        properties.SetProperties("group.id", groupId);
-        properties.SetProperties("auto.offset.reset", "earliest");
+        properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("value.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("group.id", groupId);
+        properties.setProperty("auto.offset.reset", "earliest");
 
         // create a consumer
         KafkaConsumer<String,String> consumer = new KafkaConsumer<>(properties);
@@ -34,7 +40,7 @@ public class ConsumerDemo {
         while(true){
             log.info("Polling");
 
-            ConsumerRecord<String,String> record = consumer.poll(Duration.ofMillis(1000));
+            ConsumerRecords<String,String> records = consumer.poll(Duration.ofMillis(1000));
 
             for(ConsumerRecord<String,String> record: records){
               log.info("Key: " + record.key() + ", Value: " + record.value());
